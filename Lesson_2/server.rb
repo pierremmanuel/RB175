a171e247671ff841ede56bf0c0d0acb1e49302e9
Pierre-Emmanuel Roussel <pierre.emmanuel.rssl@gmail.com>
2022-02-07 19:07:24 -0600

    commit lesson 2

---
+++ Lesson_2/server.rb
@@ -0,0 +1,16 @@
+require "socket"
+
+server = TCPServer.new("localhost", 3003)
+loop do
+  client = server.accept
+
+  request_line = client.gets
+  puts request_line
+
+
+  client.puts "HTTP/1.1 200 OK"
+  client.puts "Content-Type: text/plain\r\n\r\n"
+  client.puts request_line
+
+  client.close
+end
